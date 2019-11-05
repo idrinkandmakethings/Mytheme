5bdc80d2b4bf0378671ae7d5e043c346f5e799a1
Jeff Rector <rector_jeffrey@hotmail.com>
2019-11-05 08:16:34 -0500

    adding template renderer

---
+++ Mytheme/Templating/TemplateRenderer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Mytheme.Dal.Dto;
+using Mytheme.Templating.TemplateTypes;
+
+namespace Mytheme.Templating
+{
+    public class TemplateRenderer
+    {
+        
+
+        public string RenderTemplateToMarkDown(Template template)
+        {
+            // generate var fields first
+            var generatedVars = new Dictionary<string, string>();
+
+            foreach (var templateVariable in template.TemplateVariables)
+            {
+                
+            }
+        }
+
+        private string RenderTemplateField(TemplateField field)
+        {
+            switch (field.FieldType)
+            {
+                case TemplateFieldType.DieRoll:
+                    break;
+                case TemplateFieldType.List:
+                    break;
+                case TemplateFieldType.RandomNumber:
+                    break;
+                case TemplateFieldType.RandomTable:
+                    break;
+                case TemplateFieldType.Template:
+                    break;
+                case TemplateFieldType.DieRoll:
+                    break;
+                case TemplateFieldType.DieRoll:
+                    break;
+                case TemplateFieldType.DieRoll:
+                    break;
+
+            }
+        }
+    }
+}
