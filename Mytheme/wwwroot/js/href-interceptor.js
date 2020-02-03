414a656f9cb8c42f713866426f62f7169dab0920
Jeff Rector <jeffrey.rector@niceincontact.com>
2020-02-03 15:48:53 -0500

    hrmmmm.

--- Mytheme/wwwroot/js/href-interceptor.js
+++ Mytheme/wwwroot/js/href-interceptor.js
@@ -2,13 +2,11 @@
 
 let electronShell = {};
 
-function interceptClickEvent(e) {
+function bracketLink(link) {
+    DotNet.invokeMethodAsync("Mytheme", "BracketLinkInterop", link);
+}
 
-    try {
-        electronShell = require('electron').shell;
-    } catch (e) {
-        isInBrowser = true;
-    }
+function interceptClickEvent(e) {
 
     var tEvent = e || window.event;
 
@@ -18,53 +16,29 @@ function interceptClickEvent(e) {
 
         var link = element.href;
 
-        if (link.indexOf('bracketlink') >= 0) {
-            console.log("intercept!");
-            event.preventDefault();
-            event.stopPropagation();
-            DotNet.invokeMethodAsync("Mytheme", "BracketLinkInterop", link, element.innerText);
-
-            if (!isInBrowser) {
-                setTimeout(function() {
-                        var path = e.target.href;
-                        ipcRenderer.sendToHost('element-clicked', path);
-                    },
-                    100);
-                return false;
-            }
-        }
-        else if (link.indexOf('http://localhost') >= 0) {
+        if (link.indexOf('http://localhost') >= 0) {
             return true;
         } else {
+
+            try {
+                electronShell = require('electron').shell;
+            } catch (e) {
+                isInBrowser = true;
+            }
+
             if (isInBrowser) {
                 event.preventDefault();
+                event.stopPropagation();
                 window.open(event.target.href, '_blank');
             } else {
                 event.preventDefault();
+                event.stopPropagation();
                 electronShell.openExternal(event.target.href);
+                return false;
             }
         }
     }
 }
-
-//https://discuss.atom.io/t/how-to-intercept-electrons-request-and-return-a-customized-response/52312/3
-//https://stackoverflow.com/questions/41068295/electron-prevent-cancel-page-navigation
-try {
-    const session  = require('electron');
-    const ses = session.session;
-
-    ses.webRequest.onBeforeRequest((details, callback) => {
-        if (details.url.indexOf('bracketlink') >= 0) {
-            callback({ cancel: true });
-        } else {
-            callback({});
-        }
-    });
-
-} catch (e) {
-    // not in electron instance...
-}
-
 
 //listen for link click events at the document level
 if (document.addEventListener) {
@@ -72,5 +46,40 @@ if (document.addEventListener) {
 } else if (document.attachEvent) {
     document.attachEvent('onclick', interceptClickEvent);
 }
+
+////https://discuss.atom.io/t/how-to-intercept-electrons-request-and-return-a-customized-response/52312/3
+////https://stackoverflow.com/questions/41068295/electron-prevent-cancel-page-navigation
+////https://github.com/electron/electron/issues/1344
+
+
+
+
+//let wc = require('electron').remote.getCurrentWebContents();
+
+//wc.executeJavaScript('console.log(\'foo\')');
+
+//wc.on('will-navigate', (e, u) => { linkIntercept(e, u)});
+//wc.on('new-window', (e, u) => { linkIntercept(e, u) });
+
+
+
+//function linkIntercept(event, url) {
+//    if (url !== wc.getURL()) {
+
+//        const shell = require('electron').shell;
+//        const webview = document.querySelector('webview');
+
+//        event.preventDefault();
+//        webview.stop();
+//        webview.getWebContents().stop();
+
+//        shell.openExternal(url);
+
+//        return false;
+//    }
+//}
+
+
+
 
 
